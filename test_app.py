db49c91d5c5c0234e1153b23cfc94aa0311686d2
o-liv-e <6184prishita@gmail.com>
2026-09-24 10:54:38 +0530

    Introduced negative testcase
    Updated expected result for count_negatives test case.

--- test_app.py
+++ test_app.py
@@ -13,7 +13,7 @@ def test_sum_list(numbers, expected):
 @pytest.mark.parametrize("numbers,expected", [
     ([], 0),
     ([1, 2, 3], 0),
-    ([-1, 0, -2], 2)
+    ([-1, 0, -2], 10)
 ])
 def test_count_negatives(numbers, expected):
     assert count_negatives(numbers) == expected
